ab4b3b190a963aa04b0c98d799175ae47c6205b3
Victoria Rudakova <vicrucann@gmail.com>
2015-03-17 13:49:38 -0400

    Writing for general array done

--- write_cached_array_chunk.m
+++ write_cached_array_chunk.m
@@ -16,20 +16,20 @@ if (cacharr.caching == 1)
     end
 else
     batchsize = ceil(cacharr.dimensions(cacharr.broken) / cacharr.nchunks);
-%     indices = zeros(1, size(cacharr.dimensions, 2)+1);
-%     indices(cacharr.broken) = batchsize*(idx_chunk-1)+1; % obtain vector of form [0 0 0 1 1 0] -> (:,:,:,1:1,:)
-%     if (idx_chunk < cacharr.nchunks)
-%         indices(cacharr.broken+1) = batchsize*idx_chunk;
-%     else
-%         indices(cacharr.broken+1) = size(cacharr.data, cacharr.broken);
-%     end
-%     expr_ind = ind2str(indices);
-%     eval(['cacharr.data' expr_ind '=chunk;']);
+    indices = zeros(1, size(cacharr.dimensions, 2)+1);
+    indices(cacharr.broken) = batchsize*(idx_chunk-1)+1; % obtain vector of form [0 0 0 1 1 0] -> (:,:,:,1:1,:)
     if (idx_chunk < cacharr.nchunks)
-       cacharr.data(:,batchsize*(idx_chunk-1)+1:batchsize*idx_chunk,:,:) = chunk;
+        indices(cacharr.broken+1) = batchsize*idx_chunk;
     else
-       cacharr.data(:,batchsize*(idx_chunk-1)+1:end,:,:) = chunk;
+        indices(cacharr.broken+1) = size(cacharr.data, cacharr.broken);
     end
+    expr_ind = ind2str(indices);
+    eval(['cacharr.data' expr_ind '=chunk;']);
+%     if (idx_chunk < cacharr.nchunks)
+%        cacharr.data(:,batchsize*(idx_chunk-1)+1:batchsize*idx_chunk,:,:) = chunk;
+%     else
+%        cacharr.data(:,batchsize*(idx_chunk-1)+1:end,:,:) = chunk;
+%     end
 end
 end
 
