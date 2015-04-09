9d36f72b2652f9fd28cc4fcc25a58b258aa7e2f5
Victoria Rudakova <vicrucann@gmail.com>
2015-04-09 11:15:06 -0400

    Proper N-d array structure file created

---
+++ CachedNDArray.m
@@ -0,0 +1,18 @@
+classdef CachedNDArray
+    %CachedNDArray - data structure that allows to deal with large
+    %N-dimensional arrays through caching method
+    %   Allows to avoid Matlab out of memory error by caching large array
+    %   into several files on hard disk and then reading the necessary
+    %   chunks using memmapfile function
+    %   The data structure is inhereted from handle abstract class which
+    %   avoids parameter by value and supports parameter by reference
+    %   2015 victoria.rudakova(at)yale.edu
+    
+    properties
+    end
+    
+    methods
+    end
+    
+end
+
