5f2a6fbedf2f50eea123d44a6a9dece3672f56ee
Victoria Rudakova <vicrucann@gmail.com>
2015-08-10 15:33:41 -0400

    discr vs cont arrays

--- test_CachedNDArray.m
+++ test_CachedNDArray.m
@@ -8,8 +8,11 @@ dims = [20 10 2];
 broken = 1;
 type = 'double';
 nchunks = 2;
+fcaching = 1;
+fdiscreet = 0;
+ini_val = 0;
 
-cnda = CachedNDArray(dims, type, broken, 'tmp', 'cache', nchunks);
+cnda = CachedNDArray(dims, type, broken, 'tmp', 'cache', nchunks, fcaching, fdiscreet, ini_val);
 for i = 1:dims(1)-1
     line1 = ones(2,dims(2),1)*i;
     line2 = -line1;
@@ -20,20 +23,29 @@ end
 cnda.flush();
 linex = cnda(19:end, :, end); % check 'end' method
 cnda(19:end, :, :) = rand(2,10,2);
- 
+
+% display the result matrix to make sure the assignment/read operator works
 for j = 1:2:dims(1)-3
     line3 = cnda(j:j+2,:,:)
 end
+
+% larger scale example for continious vs. discreet methods
 fprintf('\nPress any key to continue on large-scale example\n');
 pause;
 
-dims = [500, 1000, 500, 50];
+dims = [10, 1000, 10, 50];
 broken = 2;
 type = 'single';
 
 tic;
-fprintf('allocation:\n');
-cnda = CachedNDArray(dims, type, broken, 'tmp-cnda', 'cache');
+fprintf('allocation for continious method:\n');
+cnda = CachedNDArray(dims, type, broken, 'tmp-cnda-cnt', 'cache', 4, 1);
+fprintf('done\n');
+toc;
+
+tic;
+fprintf('allocation for discreet method:\n');
+cnda_= CachedNDArray(dims, type, broken, 'tmp-cnda-dsc', 'cache', 4, 1, 1);
 fprintf('done\n');
 toc;
 
@@ -48,26 +60,39 @@ fprintf('done\n');
 toc;
 
 tic;
-fprintf('assignment operator...');
+fprintf('assignment operator for continious method...');
 cnda(:,1:floor(dims(broken) / clen),:,:) = chunk;
+fprintf('done\n');
+toc;
+
+tic;
+fprintf('assignment operator for discreet method (includes flush)...');
+cnda_(:,1:floor(dims(broken) / clen),:,:) = chunk;
 fprintf('done\n');
 toc;
 
 %flush
 tic;
-fprintf('flushing operator...')
+fprintf('flushing operator (continious method)...')
 cnda.flush();
 fprintf('done\n');
 toc;
 
 % read
 tic;
-fprintf('reference operator...');
+fprintf('reference operator (cont)...');
 %chunk_x = cnda(:, 1:floor(dims(broken) / clen),:,:);
-chunk_x = cnda(1,1,:,:);
+chunk_x = cnda(1,10,:,:);
 fprintf('done\n');
 toc;
 
+% read
+tic;
+fprintf('reference operator (discr)...');
+%chunk_x = cnda(:, 1:floor(dims(broken) / clen),:,:);
+chunk_x = cnda_(1,10,:,:);
+fprintf('done\n');
+toc;
 %if (~isequal(chunk_x, chunk))
 %    error('Matrix equality failed');
 %end
